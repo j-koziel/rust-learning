ea6e9cee4fc3193eebcf7e6a473af64a4de92591
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-06-22 22:26:26 +0100

    floating point numbers

--- common-programming-concepts/src/data_types.rs
+++ common-programming-concepts/src/data_types.rs
@@ -20,4 +20,12 @@ pub fn main() {
     // unsigned: 0 to (2^n) - 1
 
     // signed: -(2^n-1) -> 2^n-1 - 1
+
+    // Floating-point types
+    // There are only 2 primitiv types for floating point numbers (f32 and f64).
+    // When using type inference, it defaults to f64 because the speed of f32 and f64 is similar on modern CPUs
+    // Also, all floating point numbers are signed.
+    // example:
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
 }
