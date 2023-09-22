f6bb3961865374271d066932ff7503d96944366d
j-koziel <jonathankoziel1@gmail.com>
2023-09-22 21:30:15 +0100

    methods in structs

--- structs/rectangles/src/main.rs
+++ structs/rectangles/src/main.rs
@@ -4,20 +4,35 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
     // println!("rect1 is {:#?}", rect1);
-    dbg!(&rect1);
+    // dbg!(&rect1);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     rect1.area()
+    // );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2))
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
